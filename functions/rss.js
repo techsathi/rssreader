25bbc676332f89bd42cc2d492e8b7b335f0fdd33
techsathi <skumar1988@gmail.com>
2021-06-11 23:52:54 +0530

    Update rss.js

--- functions/rss.js
+++ functions/rss.js
@@ -26,7 +26,7 @@ async function getPosts() {
       method: "POST",
       headers: {
         "user-agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.212 Safari/537.36 TechSatNews",
-        "accept-encoding": "gzip, deflate",
+        //"accept-encoding": "gzip, deflate",
         "content-type": "application/json"
       }
     };
